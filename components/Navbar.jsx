6776068cc699c0b45d6ec6fcabd698061e3ad957
Abbe <abbe@Abbe.local>
2022-09-04 20:53:43 +0200

    content change to trainers

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <h1 className="logo">
-        <Link href="/">Smooth Headphones</Link>
+        <Link href="/">Smooth</Link>
       </h1>
       <button
         type="button"
