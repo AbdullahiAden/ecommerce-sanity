a9ae20c7232ddb6a0ebd82b64d9b5c1ef6b7bcae
Abbe <abbe@Abbe.local>
2022-09-04 20:55:17 +0200

    success page with confetti

---
+++ pages/success.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+
+import { useStateContext } from "../context/StateContext";
+import { runFireworks } from "../lib/utils";
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runFireworks();
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg"> check out your email for the receipt</p>
+
+        <p className="description">
+          feel free to ask questions if you have any
+          <a className="email" href="mailto:order@example.com">
+            order@example.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" className="btn" width="300">
+            Continue Shoppping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
